30c3f82d45ff3eaa73a20da09b132576d3f62fdb
elizabeth <ellizzabbetth@gmail.com>
2020-07-23 06:45:53 -0400

    test CI

--- hacker-news/Startup.cs
+++ hacker-news/Startup.cs
@@ -53,9 +53,9 @@ namespace hacker_news
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-  app.UseDeveloperExceptionPage();
+           app.UseDeveloperExceptionPage();
            app.UseHttpsRedirection();
-      //   app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+           //   app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
@@ -63,7 +63,7 @@ namespace hacker_news
             }
 
             app.UseRouting();
-              // app.UseDeveloperExceptionPage();
+            
             app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseEndpoints(endpoints =>
             {
